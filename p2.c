af96aeaadd28070a9c3b77531878e27b1235d4d6
KyKri <krkrick@gmail.com>
2016-10-10 13:21:30 -0700

    added in/outfile handling & error checking. Fixed kidpid reporting. cleaned up firstword designation.

--- p2.c
+++ p2.c
@@ -63,21 +63,22 @@ int main(){
 	for(;;){
 		prompt();
 		parse();
-		if( c == EOF )
+		if( /* *firstword == EOF */ c == EOF )
 			break;
 		if( numwords == 0 )
 			continue;
 		else{
 			if( firstword == NULL ){
-				(void) printf("Command not found.\n");
+				(void) printf("NULL Command not found.\n");
 				continue;
 			}/*handle the cd command*/
 			else if( (strcmp(firstword, "cd")) == 0 ){
-				/*If cd has no arguments, set its path to $HOME*/
+				/*Make sure cd has only 1 arg*/
 				if( newargc > 2 ){
 					(void) printf("cd received too many arguments\n");
 					continue;
-				}else if (newargv[1] == NULL){
+				}/*If cd has no arguments, set its path to $HOME*/
+				else if (newargv[1] == NULL){
 					/*make sure HOME is defined*/
 					if ( (getenv("HOME")) == NULL ){
 						(void) printf("HOME variable not defined.\n");
@@ -103,10 +104,16 @@ int main(){
 			}else{
 				/*background handler*/
 				if ( (strcmp(lastword, "&")) == 0/*background*/ ){
-					(void) printf("%s [%d]\n", newargv[0], getpid());
+					(void) printf("%s [%d]\n", newargv[0], kidpid);
 					continue;
 				}else{
-					continue;
+					for(;;){
+						int pid;
+						CHK(pid = wait(NULL));
+						if (pid == kidpid){
+							break;
+						}
+					}
 				}
 			}
 		}
@@ -141,6 +148,10 @@ void parse(){
 		else if( c == 0 )
 			break;
 		else{
+			/*if (numwords+1 = MAXITEMS){
+				(void) printf("Too many args.\n");
+				break;
+			}*/
 			*(word + numwords) = lineptr;
 			int i;
 			for( i=0; i<c; i++){
@@ -164,6 +175,10 @@ void parse(){
 			}else{ /*TO-DO: flag infile (and error checking)*/
 				inptr = word[i];
 				lastword = word[i];
+				if( i+1 < MAXITEM ){
+					infile = word[++i];
+					lastword = word[i];
+				}
 			}
 		}else if ( (strcmp( *(word + i), ">")) == 0 ){
 			if ( outptr != NULL ){
@@ -172,6 +187,10 @@ void parse(){
 			}else{ /*TO-DO: flag outfile (and error checking)*/
 				outptr = word[i];
 				lastword = word[i];
+				if( i+1 < MAXITEM ){
+					outfile = word[++i];
+					lastword = word[i];
+				}
 			}
 		/*}else if ( (strcmp( *(word + i), "$")) == 0 ){
 			;*/
@@ -186,17 +205,8 @@ void parse(){
 				lastword = word[i];
 			}
 		}else{
-			if ( firstword == NULL ){
-				if ( lastword == NULL ){
-					firstword = word[i];
-				}
-				else if ( (strcmp( lastword, "<") == 0) ||
-				 	(strcmp( lastword, ">") == 0)){
-					lastword = word[i];
-					continue;
-				}else{
-					firstword = word[i];
-				}
+			if( firstword == NULL ){
+				firstword = word[i];
 			}
 			newargv[newargc++] = word[i];
 			newargv[newargc] = '\0';
